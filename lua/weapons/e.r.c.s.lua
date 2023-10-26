4e1708ab22b273791dc92f281f37983ea69631f1
el_patito_loco <krlota123456789@gmail.com>
2023-10-26 20:03:13 +0200

    Removed unesecary parts, got some usefull comments , got done some calibration to the bullets and still having an issue with the kill feed

--- lua/weapons/e.r.c.s.lua
+++ lua/weapons/e.r.c.s.lua
@@ -9,19 +9,28 @@ At the moment our engineers are working on new features which will provide more 
 SWEP.Spawnable = true
 SWEP.AdminOnly = false
 
+SWEP.ViewModel = "models/weapons/v_357.mdl"
+SWEP.WorldModel = "models/weapons/w_357.mdl"
+
+local ShootSound = Sound("weapons/mortar/mortar_fire1.wav")
+
 SWEP.FiresUnderwater = true
 SWEP.DrawWeaponInfoBox = true
 SWEP.IconOverride = "materials/entities/weapon_357.png"
 
-SWEP.Primary.ClipSize = 10
-SWEP.Primary.DefaultClip = 10
+--ammo
+
+SWEP.Primary.ClipSize = -1
+SWEP.Primary.DefaultClip = -1
 SWEP.Primary.Automatic = true
-SWEP.Primary.Ammo = "357"
+SWEP.Primary.Ammo = "none"
 
 SWEP.Secondary.ClipSize = -1
 SWEP.Secondary.DefaultClip = -1
 SWEP.Secondary.Automatic = true
 SWEP.Secondary.Ammo = "none"
+
+--Inventory related
 
 SWEP.Weight = 5
 SWEP.AutoSwitchTo = false
@@ -32,24 +41,13 @@ SWEP.SlotPos = 3
 SWEP.DrawAmmo = false
 SWEP.DrawCrosshair = true
 
-SWEP.ViewModel = "models/weapons/v_357.mdl"
-SWEP.WorldModel = "models/weapons/w_357.mdl"
-
-local ShootSound = Sound("weapons/mortar/mortar_fire1.wav")
-
---SWEP.LoadSound = "weapons/ammopickup.wav" FOR SECONDARY FIRE
+--weapon properties
 
 SWEP.Primary.Recoil = 1
 SWEP.Primary.Damage = 75
-SWEP.Primary.NumShots = 1
+SWEP.Primary.NumShots = 10
 SWEP.Primary.Spread = 0
---SWEP.Primary.Cone = 0.25 THIS IS FOR FUTURE SCOPE
 SWEP.Primary.Delay = 1
-SWEP.Tracer = 4
---SWEP.Penetration = true
---SWEP.Ricochet = true
---SWEP.MaxRicochet = 100
---SWEP.Scoped = false
 SWEP.MouseWasDown = true
 
 function SWEP:Initialize()
@@ -79,14 +77,11 @@ function SWEP:PrimaryAttack()
 	self:ShootEffects()
 	self:EmitSound(ShootSound)
 	self.BaseClass.ShootEffects(self)
-	--self:DrawAmmo(1)
-	--self:DrawCrosshair(1)
 	self:SetNextPrimaryFire(CurTime() + self.Primary.Delay)
 end
 
-if CLIENT then --I got thjis from kilo swep hope you don't mind
-	killicon.Add("E_R_C_S", "vgui/face/grin", Color(255, 255, 255, 255))
-	--SWEP.WepSelectIcon = surface.GetTextureID("vgui/gmod_tool")
+if CLIENT then
+	killicon.AddFont("blue_shotgun", "HL2MPTypeDeath", "0", Color(0, 0, 255, 255))
 end
 
 function SWEP:SecondaryAttack() end
