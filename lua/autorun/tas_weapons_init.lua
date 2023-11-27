3a8d17be79907694748d3b412a04623da942ce29
el_patito_loco <130762237+ELPatitoLoco@users.noreply.github.com>
2023-11-27 18:01:11 +0100

    Swep el patito e.r.c.s (#17)
    * V1.0 Just uploading the basic coede
    
    * Uploading code
    
    * Changing E.R.C.S.lua name to not create conflicts
    
    * Rename E.R.C.S.lua to e.r.c.s.lua
    
    Changing E.R.C.S.lua file name to lower case to not cause conflicts with linux
    
    * Fixed lots of unused stuff and also added recoil
    
    * Removed and commented a bunch of unused functions
    and variables and changed random math having constant value
    
    * Ultimate Commenting and fixing
    
    * Fixed a bunch of things, worldmodel not showing,
    recoil, No Icon in spawnmenu, spelling errors, and some others
    
    * Removed unesecary parts, got some usefull comments
    , got done some calibration to the bullets and
    still having an issue with the kill feed
    
    * Made the kill message functional by adding a
    killicon.AddFont
    
    * Removed a bunch of useless code and comments
    
    * Added more accurate comments

--- lua/autorun/tas_weapons_init.lua
+++ lua/autorun/tas_weapons_init.lua
@@ -1,4 +1,4 @@
 TASWeapons = {
 	Category = "TAS Weapons",
-	Authors = { Derpius = "Derpius", Kilo = "Kilo" }, --STEAM_0:0:547731733 Derpius, STEAM_0:0:84051936 Kilo
+	Authors = { Derpius = "Derpius", Kilo = "Kilo", El_Patito = "el_patito" }, --STEAM_0:0:547731733 Derpius, STEAM_0:0:84051936 Kilo, STEAM_0:0:572655757 el_patito_loco
 }
